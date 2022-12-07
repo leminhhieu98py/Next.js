a4da8dca099e6e5cd56db8040a37f0bb7b6727a3
otis <leminhhieu98py@gmail.com>
2022-12-07 11:50:44 +0700

    feat: Add description field in typing and data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,22 @@ import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 import { IMeetup } from '../typings/meetup/meetup';
 
-const MOCK_MEETUPS: IMeetup[] = [
+export const MOCK_MEETUPS: IMeetup[] = [
   {
     id: 1,
     image:
       'https://images.pexels.com/photos/533769/pexels-photo-533769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     title: 'First meet up',
-    address: 'First meet up address'
+    address: 'First meet up address',
+    description: 'Description 1'
   },
   {
     id: 2,
     image:
       'https://images.pexels.com/photos/257499/pexels-photo-257499.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     title: 'Second meet up',
-    address: 'Second meet up address'
+    address: 'Second meet up address',
+    description: 'Description 2'
   }
 ];
 
--- typings/meetup/meetup.d.ts
+++ typings/meetup/meetup.d.ts
@@ -1,13 +1,10 @@
-export interface IMeetup {
-  id: number;
-  image: string;
-  title: string;
-  address: string;
-}
-
 export interface IMeetupData {
   image: string;
   title: string;
   address: string;
   description: string;
+}
+
+export interface IMeetup extends IMeetupData {
+  id: number;
 }
